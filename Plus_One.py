969287455168eca73547a5ec62b648e434559dea
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-02-18 23:15:03 +0530

    Plus_One.py

--- Plus_One.py
+++ Plus_One.py
@@ -1,6 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]==9:digits[i]=0
-            else:digits[i]=digits[i]+1;return digits
-        return [1]+digits
+        for i in range(len(digits)-1,-1,-1):#iterating till digit's length 
+            if digits[i]==9:digits[i]=0#assigning digits[0] val and 9th index val pos and digits
+            else:digits[i]=digits[i]+1;return digits#printing the incremented digits[ndx]
+        return [1]+digits#printing digit's 2nd th idx pos
